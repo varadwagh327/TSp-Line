4bba1d52b48f997b96ab5c39427891ca4ab31a2c
Varad Wagh <146649167+varadwagh327@users.noreply.github.com>
2024-11-17 17:58:52 +0000

    sda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ useEffect(() => {
   const fetchUser = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/user/me",
+        "https://tsp-line.onrender.com/api/v1/user/user/me",
         {
           withCredentials: true,
         }
